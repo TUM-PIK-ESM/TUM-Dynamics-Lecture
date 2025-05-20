939ec22d962a4060cf08352286195a90d0e9bcee
Alistair White <alistair@pik-potsdam.de>
2025-05-20 16:30:36 +0200

    Add lecture 4

--- lectures/lecture-4/interactive_plots.jl
+++ lectures/lecture-4/interactive_plots.jl
@@ -5,6 +5,7 @@ using DynamicalSystems
 using OrdinaryDiffEq
 using GLMakie
 
+# 1. Trajectories of the Lorenz system
 lorenz = Systems.lorenz()
 
 parameter_sliders = Dict(
@@ -14,16 +15,23 @@ parameter_sliders = Dict(
 )
 parameter_names = Dict(1 => "σ", 2 => "ρ", 3 => "β")
 
-fig, dsobs = interactive_trajectory(lorenz, [rand(3), rand(3)]; add_controls=true, parameter_sliders, parameter_names)
+fig, dsobs = interactive_trajectory(
+    lorenz,
+    [rand(3), rand(3)];
+    add_controls=true,
+    parameter_sliders,
+    parameter_names,
+    lims=((-30, 30), (-30, 30), (0, 50)),
+    )
 fig
 
+# 2. Orbit diagram of the Henon map
 henon = Systems.henon()
 
 i = 1                    # First variable
 p_index = 1              # First parameter
-p_min, p_max = 0.8, 1.4  # Min,max value of parameter
+p_min, p_max = 0.8, 1.4  # Min, max value of parameter
 
 title = "Orbit Diagram for the Hénon map"
-
-fig, dsobs = interactive_orbitdiagram(henon, p_index, p_min, p_max, i, parname = "a", title = title)
+fig, dsobs = interactive_orbitdiagram(henon, p_index, p_min, p_max, i, parname="a", title=title)
 fig
